34b8156f4f4228fb6debe13869ea18c355142be2
rrifafauzikomara <rifafauzi6@gmail.com>
2022-11-17 01:19:46 +0700

    remove unused code

--- lib/data/seafood_response.dart
+++ lib/data/seafood_response.dart
@@ -5,13 +5,10 @@ class SeafoodResponse {
 
   final List<Meal> meals;
 
-  factory SeafoodResponse.fromJson(Map<String, dynamic> json) => SeafoodResponse(
-    meals: List<Meal>.from(json["meals"].map((x) => Meal.fromJson(x))),
-  );
-
-  Map<String, dynamic> toJson() => {
-    "meals": List<Meal>.from(meals.map((x) => x.toJson())),
-  };
+  factory SeafoodResponse.fromJson(Map<String, dynamic> json) =>
+      SeafoodResponse(
+        meals: List<Meal>.from(json["meals"].map((x) => Meal.fromJson(x))),
+      );
 }
 
 class Meal {
@@ -26,14 +23,8 @@ class Meal {
   final String idMeal;
 
   factory Meal.fromJson(Map<String, dynamic> json) => Meal(
-    strMeal: json["strMeal"],
-    strMealThumb: json["strMealThumb"],
-    idMeal: json["idMeal"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "strMeal": strMeal,
-    "strMealThumb": strMealThumb,
-    "idMeal": idMeal,
-  };
+        strMeal: json["strMeal"],
+        strMealThumb: json["strMealThumb"],
+        idMeal: json["idMeal"],
+      );
 }
